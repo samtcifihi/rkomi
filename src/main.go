90eb7bcd3d5e0d559377a417a57d4c2543e2fa82
samtcifihi <la.lojban@gmail.com>
2022-01-21 12:02:16 -0500

    initial high-level outline

--- src/main.go
+++ src/main.go
@@ -7,6 +7,14 @@ import (
 )
 
 func main() {
-	test := os.Args[0]
+	test := os.Args[0] // DEBUG
 	fmt.Println(test, "ran successfuly.") // DEBUG
+
+	// parse arguments
+
+	// convert strengths to internal rating as in pointsratingsystem
+
+	// calculate reverse komi and/or handicap stones
+
+	// output
 }
